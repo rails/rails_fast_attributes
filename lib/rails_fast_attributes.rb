afae80ca816b52e96b589560de105beb13a0ba35
Sean Griffin <sean@seantheprogrammer.com>
2017-11-14 07:01:38 -0700

    Leave some constants where Ruby expects to find them

--- lib/rails_fast_attributes.rb
+++ lib/rails_fast_attributes.rb
@@ -1,9 +1,13 @@
 require "helix_runtime"
 require "rails_fast_attributes/native"
 require "rails_fast_attributes/version"
+require "active_record"
+require "active_record/attributes"
+require "active_record/relation/query_attribute"
 
 module RailsFastAttributes
   ORIGINAL_ATTRIBUTE = ActiveRecord::Attribute
+  Attribute::UserProvidedDefault = ORIGINAL_ATTRIBUTE::UserProvidedDefault
 
   def Attribute.inherited(*)
     raise "ActiveRecord::Attribute cannot be subclassed when using rails_fast_attributes"
